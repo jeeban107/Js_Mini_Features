e20df77c9be36c26f5cccf0cd399d7494167ffe9
jeeban107 <jeevanbehera107@gmail.com>
2024-07-02 16:12:51 +0530

    Cursor
    Done

--- Cursor/script.js
+++ Cursor/script.js
@@ -1,2 +1,11 @@
-var cursor = document.querySelector(".cursor");
 
+var main = document.querySelector(".main");
+var crsr = document.querySelector(".cursor");
+
+
+main.addEventListener("mousemove",function(dets){
+
+    crsr.style.left=dets.x +"px"
+    crsr.style.top=dets.y +"px"
+
+})

5ef1524687bfa716913c0d3233eb13c234c15640
jeeban107 <jeevanbehera107@gmail.com>
2024-07-12 22:59:42 +0530

    trottling
    function added

---
+++ Image_Dance/script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector("#throttle");
+
+const throttleFunction = (func, delay) => {
+  let prev = 0;
+  return (...args) => {
+    let now = new Date().getTime();
+
+    console.log(now - prev, delay);
+
+    if (now - prev > delay) {
+      prev = now;
+
+      return func(...args);
+    }
+  };
+};
+btn.addEventListener(
+  "click",
+  throttleFunction(() => {
+    console.log("button is clicked");
+  }, 1500)
+);

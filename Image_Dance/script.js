360421c5ab65b419a1d4683ace44fe03b197bce5
jeeban107 <jeevanbehera107@gmail.com>
2024-07-12 23:21:08 +0530

    ds
    s

--- Image_Dance/script.js
+++ Image_Dance/script.js
@@ -4,12 +4,8 @@ const throttleFunction = (func, delay) => {
   let prev = 0;
   return (...args) => {
     let now = new Date().getTime();
-
-    console.log(now - prev, delay);
-
     if (now - prev > delay) {
       prev = now;
-
       return func(...args);
     }
   };
